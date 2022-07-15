765923293b853d56afc876e13269c72ffd4afaaa
Takeshi Kondo <takeshi.kondo@gmail.com>
2022-07-15 17:47:01 +0900

    update

--- src/main/java/com/yourorg/A.java
+++ src/main/java/com/yourorg/A.java
@@ -1,5 +1,12 @@
 package com.yourorg;
 
 class A {
+    public String hi() {
+        return "Hello from com.yourorg.A!";
+    }
+
+    public String hello() {
+        return "Hello from com.yourorg.A!";
+    }
 
 }
